aedd103d1b29a912b9969998cc6a78ee5a410ed3
Dean <deangotto@outlook.com>
2022-06-30 15:29:34 -0700

    starter files

---
+++ passportStarter/models/userModel.js
@@ -0,0 +1,39 @@
+const database = [
+  {
+    id: 1,
+    name: "Jimmy Smith",
+    email: "jimmy123@gmail.com",
+    password: "jimmy123!",
+  },
+  {
+    id: 2,
+    name: "Johnny Doe",
+    email: "johnny123@gmail.com",
+    password: "johnny123!",
+  },
+  {
+    id: 3,
+    name: "Jonathan Chen",
+    email: "jonathan123@gmail.com",
+    password: "jonathan123!",
+  },
+];
+
+const userModel = {
+  findOne: (email) => {
+    const user = database.find((user) => user.email === email);
+    if (user) {
+      return user;
+    }
+    throw new Error(`Couldn't find user with email: ${email}`);
+  },
+  findById: (id) => {
+    const user = database.find((user) => user.id === id);
+    if (user) {
+      return user;
+    }
+    throw new Error(`Couldn't find user with id: ${id}`);
+  },
+};
+
+module.exports = { database, userModel };
---
+++ passportStarter/routes/authRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const passport = require("../middleware/passport");
+const { forwardAuthenticated } = require("../middleware/checkAuth");
+
+const router = express.Router();
+
+router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/auth/login",
+  })
+);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auth/login");
+});
+
+module.exports = router;
